2c39b2c9d0ad73a9878e4a6eb2e1b109efdc2867
Louvain team <unknown>
2012-01-26 09:59:24 +0100

    Changing license to GPL

--- main_hierarchy.cpp
+++ main_hierarchy.cpp
@@ -5,7 +5,20 @@
 // Based on the article "Fast unfolding of community hierarchies in large networks"
 // Copyright (C) 2008 V. Blondel, J.-L. Guillaume, R. Lambiotte, E. Lefebvre
 //
-// This program must not be distributed without agreement of the above mentionned authors.
+// This file is part of Louvain algorithm.
+// 
+// Louvain algorithm is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Louvain algorithm is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Louvain algorithm.  If not, see <http://www.gnu.org/licenses/>.
 //-----------------------------------------------------------------------------
 // Author   : E. Lefebvre, adapted by J.-L. Guillaume
 // Email    : jean-loup.guillaume@lip6.fr
